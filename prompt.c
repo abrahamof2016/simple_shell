acfd00b2e246b188920c735e04c965881f9500fa
abrahamof2016 <jarageda9@gmail.com>
2023-10-18 18:56:53 +0000

    displays a prompt

---
+++ prompt.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+/**
+  * prompt - displays a prompt and 
+  * wait for the user to type a command.
+  *
+  * Return: nothing.
+  */
+void prompt(void)
+{
+	if ((isatty(STDIN_FILENO) == 1) && (isatty(STDOUT_FILENO) == 1))
+		write(STDERR_FILENO, "$ ", 2);
+}
